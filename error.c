1205cb3139db26ff08f508dc0c128b02dff37f87
Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
2013-08-04 00:00:00 +0000

    added macros for errors

--- error.c
+++ error.c
@@ -5,10 +5,13 @@ This file is part of Kappa. */
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+#include <assert.h>
 #include "trace.h"
 #include "error.h"
 
 static const size_t MESSAGE_MAX_LENGTH = 256;
+static const char *ARGUMENT_NULL = "Null is not a valid value.\n";
 
 static void error_trace(const char *message, va_list varg) {
     char string[MESSAGE_MAX_LENGTH];
@@ -22,5 +25,45 @@ void error(int quit, const char *message, ...) {
     error_trace(format, vargs);
     va_end(vargs);
     if(quit == 1) exit(EXIT_FAILURE);
+}
+
+void fail(void *args) {
+    abort();
+}
+
+void null(void *args) {
+    fprintf(stderr, "%s", ARGUMENT_NULL);
+    fail(args);
+}
+
+void err(args args) {
+    if(args.message) fprintf(stderr, args.format ? args.format : "%s", args.message);
+    if(args.func) args.func(args.arg);
+}
+
+char *format(char const *format, ...) {
+    char *output;
+    va_list list;
+    va_start(list, format);
+    size_t size = vsnprintf(NULL, 0, format, list) + 1;
+    if((output = malloc(size)) == NULL) return(NULL);
+    vsnprintf(output, size, format, list);
+    va_end(list);
+    return(output);
+}
+
+void concat(char **target, char const *source) {
+    if(!source) { fprintf(stderr, "Null is not a valid value.\n"); exit(1); }
+    if(*target) {
+        char buffer[strlen(*target) + 1];
+        strcpy(buffer, *target);
+        free(*target);
+        *target = malloc(strlen(buffer) + strlen(source) + 1);
+        strcpy(*target, buffer);
+        strcat(*target, source);
+    } else {
+        *target = malloc(strlen(source) + 1);
+        strcpy(*target, source);
+    }
 }
 
--- error.h
+++ error.h
@@ -5,6 +5,26 @@ This file is part of Kappa. */
 #ifndef ERROR_H
 #define ERROR_H
 
+#define ensure(value, ...) if(!value) err((args){__VA_ARGS__})
+
+#define error(s)\
+    fprintf(\
+        stderr, "e: %s, n: %d, s: [%s], f: %s, l: %d\n", \
+        s, errno, strerror(errno), __FILE__, __LINE__\
+    )\
+
+typedef struct args {
+    void (*func)(void *);
+    void *arg;
+    char const *format;
+    char const *message;
+} args;
+
+void fail(void *);
+void null(void *);
+void err(args);
+char *format(char const *format_info, ...);
+void concat(char **target, char const *source);
 void error(int quit, const char *message, ...);
 
 #endif

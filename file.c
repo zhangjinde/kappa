1097beef7eb4730b701784defc17ea72e8cf36f3
Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
2015-10-08 00:00:00 +0000

    simplified simple file wrapper utility routines

--- file.c
+++ file.c
@@ -1,68 +1,67 @@
-/* simple io utility structures and routines
-Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
-This file is part of Kappa. */
+/*
+* file io wrapper utility structures and routines
+* Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
+* This file is part of Kappa.
+*/
 
 #include <stdio.h>
 #include <errno.h>
 #include <assert.h>
-#include "sio_error.h"
-#include "sio_errors.h"
-#include "sio_file.h"
+#include "error.h"
+#include "file.h"
 
-static int finally_file_close(struct closure *closure) {
-    assert(closure);
-    assert(closure->argv);
+static int file_close(struct closure *closure) {
     FILE *stream = *closure->argv;
-
-    if(stream && (fclose(stream))) return error(stream_close_failed);
+    if (stream && (fclose(stream))) return error(stream_close_failed);
     return 0;
 }
 
-int file_exists(const char *url) {
+int file_exists(const char *path) {
     FILE *stream = NULL;
     const char *mode = "rb";
-
     struct closure finally = { .argv = (void *[]){ stream }, .func = finally_file_close };
 
-    if(!(stream = fopen(url, mode))) switch(errno) {
+    if (!(stream = fopen(path, mode))) switch(errno) {
         case ENOENT: return 0;
-        default: return error(file_open_failed, url, mode);
+        default: return error(file_open_failed, path, mode);
     }
 
     finally.func(&finally);
     return 1;
 }
 
-int file_is_empty(const char *url) {
+int file_is_empty(const char *path) {
+    int e;
     FILE *stream = NULL;
     const char *mode = "rb";
+    struct closure finally = {
+        .argv = (void *[]){ stream },
+        .func = finally_file_close
+    };
 
-    struct closure finally = { .argv = (void *[]){ stream }, .func = finally_file_close };
-
-    int rv;
-    if(!(stream = fopen(url, mode))) switch(errno) {
+    if (!(stream = fopen(path, mode))) switch(errno) {
         case ENOENT: return 1;
-        default: return error(file_open_failed, url, mode);
+        default: return error(file_open_failed, path, mode);
     }
-    if((fseek(stream, 0, SEEK_END))) return error_finally(&finally, stream_seek_failed, url);
-    if((rv = ftell(stream)) < 0) return error_finally(&finally, stream_tell_failed, url);
+    if ((fseek(stream, 0, SEEK_END))) return error_finally(&finally, stream_seek_failed, path);
+    if ((e = ftell(stream)) < 0) return error_finally(&finally, stream_tell_failed, path);
 
     finally.func(&finally);
-    return (rv ? 0 : 1);
+    return (e ? 0 : 1);
 }
 
-int file_create(const char *url) {
+int file_create(const char *path) {
     FILE *stream = NULL;
     const char *mode = "w+b";
-
     struct closure finally = { .argv = (void *[]){ stream }, .func = finally_file_close };
-    if(!(stream = fopen(url, mode))) return error(file_open_failed, url, mode);
+
+    if (!(stream = fopen(path, mode))) return error(file_open_failed, path, mode);
     finally.func(&finally);
     return 0;
 }
 
-int file_remove(const char *url) {
-    if((remove(url))) return error(file_remove_failed, url);
+int file_remove(const char *path) {
+    if ((remove(path))) return error(file_remove_failed, path);
     return 0;
 }
 
--- file.h
+++ file.h
@@ -1,14 +1,16 @@
-/* simple io utility structures and routines
-Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
-This file is part of Kappa. */
+/*
+* file io wrapper utility structures and routines
+* Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
+* This file is part of Kappa.
+*/
 
-#ifndef SIO_FILE_H
-#define SIO_FILE_H
+#ifndef FILE_H
+#define FILE_H
 
-int file_exists(const char *url);
-int file_is_empty(const char *url);
-int file_create(const char *url);
-int file_remove(const char *url);
+int file_exists(const char *path);
+int file_is_empty(const char *path);
+int file_create(const char *path);
+int file_remove(const char *path);
 
 #endif
 

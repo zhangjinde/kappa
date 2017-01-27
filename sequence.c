f36ad0385773d0ff095f039d18eed96d6027b9e1
Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
2017-01-26 18:20:39 -0600

    provided synchronous multiplex i/o for sequence consumers

--- sequence.c
+++ sequence.c
@@ -10,9 +10,12 @@
 #include <string.h>
 #include <errno.h>
 #include <signal.h>
+#include <pthread.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/time.h>
+#include <sys/select.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include "error.h"
@@ -20,8 +23,60 @@
 #include "stream.h"
 #include "sequence.h"
 
-static int socket_close(int fd) {
-    if (close(fd)) return -1;
+static int seqnum = 0x30;
+static int consumerfds[8] = { 0 };
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+static int socket_accept(int pfd, int *afd);
+
+static int get_nfds() {
+    int *fd;
+    for (fd = consumerfds; *fd; fd++);
+    return (fd - consumerfds) ? (*--fd + 1) : 0;
+}
+
+static int provide_sequence() {
+    int e, fd, *cfd, nfds = 0;
+    fd_set readfds;
+    ssize_t nbr, nbw;
+    int wbf[] = { 0 };
+    char rbf[] = { 0 };
+
+    for (;;) {
+        if ((e = pthread_mutex_lock(&mutex))) return -1;
+
+        while (!(nfds = get_nfds())) pthread_cond_wait(&cond, &mutex);
+
+        FD_ZERO(&readfds);
+        for (cfd = consumerfds; *cfd; cfd++) FD_SET(*cfd, &readfds);
+
+        if ((e = pthread_mutex_unlock(&mutex))) return -1;
+        if ((e = select(nfds, &readfds, NULL, NULL, NULL)) == -1) return -1;
+
+        for (fd = 0; fd < nfds; fd++) {
+            if (FD_ISSET(fd, &readfds)) {
+                wbf[0] = seqnum;
+                if ((e = stream_read(fd, rbf, sizeof rbf, &nbr))) return -1;
+                if ((e = stream_write(fd, wbf, sizeof wbf, &nbw))) return -1;
+                ++seqnum;
+            }
+        }
+    }
+}
+
+static int accept_consumer(int pfd) {
+    for (int afd, e; ; afd = e = 0) {
+        e = socket_accept(pfd, &afd);
+        if ((e = pthread_mutex_lock(&mutex))) return -1;
+        for (int *cfd = consumerfds; *cfd; cfd++);
+        *cfd = afd;
+        if ((e = pthread_mutex_unlock(&mutex))) return -1;
+        if ((e = pthread_cond_signal(&cond))) return -1;
+    }
+}
+
+static int socket_close(int pfd) {
+    if (close(pfd)) return -1;
     return 0;
 }
 
@@ -30,7 +85,7 @@ static int socket_accept(int pfd, int *afd) {
     socklen_t addrlen = sizeof(addr);
     *afd = accept(pfd, (struct sockaddr *)&addr, &addrlen);
     if (*afd < 0 && errno != EINTR) return -1;
-    return *afd;
+    return 0;
 }
 
 static int socket_listen(int fd, int queue) {
@@ -45,10 +100,14 @@ static int socket_bind(int fd, struct sockaddr_in *addr) {
 
 static int socket_make(int *fd) {
     if ((*fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) return -1;
-    return *fd;
+    return 0;
 }
 
-static int address_make(struct sockaddr_in *addr, const char *host, unsigned short port) {
+static int address_make(
+    struct sockaddr_in *addr,
+    const char *host,
+    unsigned short port
+) {
     memset(addr, 0, sizeof(*addr));
     addr->sin_family = AF_INET;
     addr->sin_port = htons(port);
@@ -59,21 +118,29 @@ static int address_make(struct sockaddr_in *addr, const char *host, unsigned sho
     }
 }
 
-int start_sequence(const char *host, unsigned short port, unsigned short queue) {
-    int e, fd, afd;
+int start_sequence(
+    const char *host,
+    unsigned short port,
+    unsigned short queue
+) {
+    int e, fd;
     struct sockaddr_in addr;
-    char seq[] = { 0x30 };
-    ssize_t nbw = 0;
+    pthread_t accept_consumers, provide_sequences;
 
     make_daemon(0, 0);
     if ((e = address_make(&addr, host, port)) == -1) return -1;
     if ((e = socket_make(&fd)) == -1) return -1;
     if ((e = socket_bind(fd, &addr)) == -1) return -1; 
     if ((e = socket_listen(fd, queue)) == -1) return -1;
-    for ( ; (e = socket_accept(fd, &afd)); ) {
-        if ((e = stream_write(afd, seq, sizeof seq, &nbw)) == -1) return -1;
-        if ((e = socket_close(afd)) == -1) return -1;
-    }
+
+    if ((e = pthread_create(&accept_consumers, NULL, accept_consumer, fd)))
+        return -1;
+
+    if ((e = pthread_create(&provide_sequences, NULL, provide_sequence, NULL)))
+        return -1;
+
+    if ((e = pthread_join(accept_consumers, NULL))) return -1;
+    if ((e = pthread_join(provide_sequences, NULL))) return -1;
     if ((e = socket_close(fd)) == -1) return -1;
 
     return 0;

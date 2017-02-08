012445ea7414560fdcdf7adbef81779edb2c3f70
Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
2017-02-08 13:08:05 -0600

    created tests and test harnesses for the fatal utility routines

---
+++ test-fatal-harness.c
@@ -0,0 +1,39 @@
+/*
+* a harness for fatal test routines
+* Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
+* This file is part of Kappa.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include "log.h"
+#include "fatal.h"
+#include "error.h"
+#include "trace.h"
+#include "debug.h"
+
+static void terminate(void) {
+    trace("terminated a process");
+}
+
+int main(int argc, const char **argv) {
+    assert(argc == 1);
+
+    const char *mod_name;
+
+    mod_name = argv[0];
+    log_init(mod_name);
+
+    if (atexit(terminate))
+        error("a termination handler could not be registered, [%s]", mod_name);
+
+    errno = 1;
+    fatal("testing a fatal condition");
+    
+    assert(0);
+}
+
---
+++ test-fatal.c
@@ -0,0 +1,49 @@
+/*
+* tests for fatal condition routines
+* Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
+* This file is part of Kappa.
+*/
+
+#include <stdlib.h>
+#include <assert.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include "log.h"
+#include "fatal.h"
+#include "error.h"
+#include "trace.h"
+#include "debug.h"
+
+static void terminate(void) {
+    trace("terminated a process");
+}
+
+static int test_fatal() {
+    int e;
+    const char *mod_name = __func__;
+    const char *prog_name = "./test-fatal-harness";
+    char shell_command[0x40] = { 0 };
+
+    memmove(shell_command, prog_name, strlen(prog_name));
+    debug("shell command text: [%s]", shell_command);
+
+    log_init(mod_name);
+
+    if (atexit(terminate))
+        error("a termination handler could not be registered, [%s]", mod_name);
+
+    e = system(shell_command);
+    assert(e == 256);
+
+    log_deinit(mod_name);
+
+    return 0;
+}
+
+int main(void) {
+    if (test_fatal()) assert(0);
+
+    exit(EXIT_SUCCESS);
+}
+

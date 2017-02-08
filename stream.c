b72db27eb6f4d85dff7f7e9a3b0e0d9c02044a14
Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
2017-02-08 13:14:03 -0600

    fixed up to use the error routines

--- stream.c
+++ stream.c
@@ -11,29 +11,41 @@
 #include "error.h"
 #include "stream.h"
 
-#define errors(err, nb) ((nb==-1) && (err!=0) && (err!=EINTR))
-#define done(err, nb) !((err==EINTR) && (nb<0))
+#define errors(e, nb) ((nb == -1) && (e != 0) && (e != EINTR))
+#define done(e, nb) !((e == EINTR) && (nb <0 ))
 
 int stream_read(int fd, void *bf, size_t nb, ssize_t *nbr) {
     *nbr = 0;
     ssize_t cnbr;
+
     while ((cnbr = read(fd, bf, nb)) != 0) {
-        if (errors(errno, cnbr)) return 1;
+        if (errors(errno, cnbr))
+            return error("reading from a file descriptor %d failed", fd);
+
         bf = (char *)bf + (cnbr ? cnbr : 0);
         *nbr += cnbr;
-        if (done(errno, cnbr)) break;
+
+        if (done(errno, cnbr))
+            break;
     }
+
     return 0;
 }
 
 int stream_write(int fd, const void *bf, size_t nb, ssize_t *nbw) {
     *nbw = 0;
     ssize_t cnbw;
+
     while ((cnbw = write(fd, bf, (nb - (size_t)*nbw))) != 0) {
-        if (errors(errno, *nbw)) return 1;
+
+        if (errors(errno, *nbw))
+            return error("writing to a file descriptor %d failed", fd);
+
         bf = (const char *)bf + (*nbw ? *nbw : 0);
         *nbw += cnbw;
-        if (done(errno, cnbw)) break;
+
+        if (done(errno, cnbw))
+            break;
     }
     return 0;
 }

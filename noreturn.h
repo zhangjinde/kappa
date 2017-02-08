edc012487831ae9a34a0c9cf96fc37ac28c14a43
Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
2017-02-08 10:18:11 -0600

    created initial macros for function and parameter specifiers

---
+++ noreturn.h
@@ -0,0 +1,14 @@
+/*
+* noreturn function specifier
+* Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
+* This file is part of Kappa.
+*/
+
+#ifndef NORETURN_H
+#define NORETURN_H
+
+#include <stdnoreturn.h>
+#define noreturn _Noreturn
+
+#endif
+
---
+++ notused.h
@@ -0,0 +1,13 @@
+/*
+* notused parameter specifier
+* Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
+* This file is part of Kappa.
+*/
+
+#ifndef NOTUSED_H
+#define NOTUSED_H
+
+#define notused(param) (void)param
+
+#endif
+

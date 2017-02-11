49f81c7f116c05b843d24de0084b700ab259b075
Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
2017-02-11 12:32:06 -0600

    renamed some objects

--- callback.h
+++ callback.h
@@ -1,15 +1,15 @@
 /*
-* closure utility structures and routines
+* callback structure
 * Copyright (C) 2013-2017 Roman Fakhrazeyev <roman.fakhrazeyev@xinoir.com>
 * This file is part of Kappa.
 */
 
-#ifndef CLOSURE_H
-#define CLOSURE_H
+#ifndef CALLBACK_H
+#define CALLBACK_H
 
-struct closure {
+struct callback {
     void **argv;
-    int (* func)(struct closure *);
+    int (* func)(struct callback *);
 };
 
 #endif
